2f360be1b00ef34b7fd543226c9bdcbcdb8ff408
Paulina Avila <paulinavila.lopez@gmail.com>
2021-10-20 16:38:28 -0500

    poo avanzado2

--- src/com/generation/Main.java
+++ src/com/generation/Main.java
@@ -6,9 +6,35 @@ import com.generation.person.Person;
 
 public class Main {
 
-    public static void main(String[] args) {
+    //clase
+    public static int numerador = 10;
+    public static int denominador = 0;
+    public static float division;
 
-        CuentaBancaria cuenta = new CuentaBancaria();
+
+    public static void main(String[] args) {
+        //método
+
+        System.out.println("Antes de hacer la division");
+
+
+        //dentro del try el código propenso a fallar
+        try {
+            division = numerador / denominador;
+            //ejecuta el código después del error
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        } finally {
+            //puede ser opcional
+            System.out.println("soy fanally");
+        }
+        //que si hay un error el programa no se detenga y que muestre el error
+
+        System.out.println("Después de la división");
+
+
+
+     /*   CuentaBancaria cuenta = new CuentaBancaria();
         Person person = new Person();
         person.setName("Paulina Avila");
         person.setAge(28);
@@ -19,7 +45,7 @@ public class Main {
         System.out.println(person.getAge());
         person.mostrarAge();
         System.out.println(person.getId());
-        person.mostrarId();
+        person.mostrarId();*/
 
 
 
